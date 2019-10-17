e64ec3d7af1f1f99a07a81606689a7cda19e52eb
YNX <yunongxin@sina.com>
2019-10-17 17:38:13 +0800

    [feature] Add io

---
+++ src/com/company/io/DirList.java
@@ -0,0 +1,53 @@
+package com.company.io;
+
+import java.io.File;
+
+/**
+ * 创建目录，读取目录，删除目录或文件
+ *
+ * @author ynx
+ * @version V1.0
+ * @date 2019-10-17
+ * @modified_date 2019-10-17
+ */
+public class DirList {
+
+    public static void main(String[] args) {
+        String dirname = "/temp/iotest";
+        File f1 = new File(dirname);
+        f1.mkdirs();
+        if (f1.isDirectory()) {
+            System.out.println("Directory of " + dirname);
+            String[] s = f1.list();
+            for (int i = 0; i < s.length; i++) {
+                File f = new File(dirname + "/" + s[i]);
+                if (f.isDirectory()) {
+                    System.out.println(s[i] + " is a directory");
+                } else {
+                    System.out.println(s[i] + " is a file");
+                }
+            }
+        }else {
+            System.out.println(dirname + " is not a directory");
+        }
+        //删除iotest文件夹
+        deleteFolder(f1);
+    }
+
+
+    //删除文件及目录
+    public static void deleteFolder(File folder) {
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    deleteFolder(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+        folder.delete();
+
+    }
+}
---
+++ src/com/company/util/DateUtils.java
@@ -0,0 +1,34 @@
+package com.company.util;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * Java 8 时间转换工具类
+ *
+ * @author ynx
+ * @version V1.0
+ * @date 2019-10-17
+ * @modified_date 2019-10-17
+ */
+public class DateUtils {
+
+    public static Date asDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date asDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDate asLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public static LocalDateTime asLocalDateTime(Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+}
