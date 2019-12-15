df1e06150bd814d67afa4f360f617709b076bd76
ynx-desktop <yunongxin@sina.com>
2019-12-15 18:23:39 +0800

    [feature] Add Adapter pattern

---
+++ src/com/company/designpattern/adapter/VlcPlayer.java
@@ -0,0 +1,19 @@
+package com.company.designpattern.adapter;
+
+/**
+ * @author ynx
+ * @version V1.0
+ * @date 2019-12-15
+ * @modified_date 2019-12-15
+ */
+public class VlcPlayer implements AdvancedMediaPlayer {
+    @Override
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: "+ fileName);
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        // do nothing
+    }
+}
