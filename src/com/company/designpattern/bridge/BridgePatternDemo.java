f284bac1b02288c417f543dea606fd6fc406a48e
ynx-desktop <yunongxin@sina.com>
2019-12-15 20:42:28 +0800

    [feature] Add Bridge pattern demo

---
+++ src/com/company/designpattern/bridge/BridgePatternDemo.java
@@ -0,0 +1,18 @@
+package com.company.designpattern.bridge;
+
+/**
+ * 桥接模式
+ * @author ynx
+ * @version V1.0
+ * @date 2019-12-15
+ * @modified_date 2019-12-15
+ */
+public class BridgePatternDemo {
+    public static void main(String[] args) {
+        Shape redCircle = new Circle(100,100, 10, new RedCircle());
+        Shape greenCircle = new Circle(50,50, 5, new GreenCircle());
+
+        redCircle.draw();
+        greenCircle.draw();
+    }
+}
---
+++ src/com/company/designpattern/bridge/RedCircle.java
@@ -0,0 +1,16 @@
+package com.company.designpattern.bridge;
+
+/**
+ * 画图的具体实现-红色圆
+ * @author ynx
+ * @version V1.0
+ * @date 2019-12-15
+ * @modified_date 2019-12-15
+ */
+public class RedCircle implements DrawApi {
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawing Circle[ color: red, radius: "
+                + radius +", x: " +x+", y: "+ y +"]");
+    }
+}
