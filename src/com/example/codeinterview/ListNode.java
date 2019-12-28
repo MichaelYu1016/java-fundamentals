61ea8ad422d1ee54d377be152f471bc728b073b2
MichaelYu1016 <1520115811@qq.com>
2019-12-28 20:20:29 +0800

    [Add] ListNode class

---
+++ src/com/example/codeinterview/ListNode.java
@@ -0,0 +1,13 @@
+package com.example.codeinterview;
+
+/**
+ * 链表结构
+ */
+public class ListNode {
+    int val;
+    // 下一个链表对象
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
